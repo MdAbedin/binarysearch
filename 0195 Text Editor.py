ed139c6de85130fad79985755b55ca60274bede4
Md Abedin <mdabedincs@gmail.com>
2021-03-12 23:44:21 -0500

    add 0195

---
+++ 0195 Text Editor.py
@@ -0,0 +1,16 @@
+class Solution:
+    def solve(self, s):
+        stack = []
+        i = 0
+
+        while i < len(s):
+            if s[i:i+2] == "<-":
+                if stack: stack.pop()
+                i += 2
+            else:
+                stack.append(s[i])
+                i += 1
+
+        answer = "".join(stack)
+
+        return answer
