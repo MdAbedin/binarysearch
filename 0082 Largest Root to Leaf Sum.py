ed1708a391edfacd118867cf36cd53dedaef0fe0
Md Abedin <mdabedincs@gmail.com>
2021-07-05 22:45:26 -0400

    add 0082

---
+++ 0082 Largest Root to Leaf Sum.py
@@ -0,0 +1,9 @@
+# class Tree:
+#     def __init__(self, val, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def solve(self, root):
+        if not root: return 0
+        return max(self.solve(root.left), self.solve(root.right)) + root.val
