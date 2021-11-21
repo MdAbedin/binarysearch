41776e2f6e79ea906909e184ba4a5c42a749b917
Md Abedin <mdabedincs@gmail.com>
2021-11-20 23:48:30 -0500

    update 0368

--- 0368 Copy Paste.py
+++ 0368 Copy Paste.py
@@ -1,4 +1,4 @@
 class Solution:
+    @lru_cache
     def solve(self, n):
-        if n <= 4: return n
-        return max(self.solve(n-4)*4, self.solve(n-3)*3)
+        return n if n <= 4 else max(self.solve(n-4)*4, self.solve(n-3)*3)
